7fe9090b6a4c5b74e75ad222217f893c82c4f84e
Jarred Hare <jarredhare@gmail.com>
2014-12-04 17:44:41 -0800

    Add ajax call to replace hidden forms

--- public/js/application.js
+++ public/js/application.js
@@ -7,17 +7,29 @@ function Controller() {
 }
 
 Controller.prototype.bindListeners = function(){
-  $('.add-book').on('click', changeAddButton)
+  $('.add-form').on('click', addBook)
+  $('.star').on('click', function(e){
+    console.log(e.target)
+  })
 }
 
-var changeAddButton = function(e){
+var addBook = function(e){
   e.preventDefault()
-  currentAddButton = $(this)
+  var form = $(this)
+  var currentAddButton = form.find('button')
+  var book = form.parents().eq(2)
+  var book_data = {title: book.find('#title').text(),
+                   author: book.find('#author').text(),
+                   description: book.find('#description').text(),
+                   img_url: book.find('#img_url').attr('src'),
+                   isbn: book.find('#isbn').text(),
+                   average_rating: book.find('#average_rating').text(),
+                   categories: book.find('#categories').text() }
   $.ajax({
     url: "/books",
     type: "post",
     dataType: "json",
-    data: currentAddButton.parent().serialize()
+    data: book_data
   }).done(function(data){
     currentAddButton.attr("disabled", "disabled")
     currentAddButton.html('Added')
